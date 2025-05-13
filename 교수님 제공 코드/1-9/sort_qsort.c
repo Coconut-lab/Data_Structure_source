48232a12cf918060623c184b81a8d2e79b7b994d
2coconut <wosjiwjs@gmail.com>
2025-05-13 17:29:07 +0900

    일부 수정

--- 교수님 제공 코드/1-9/sort_qsort.c
+++ 교수님 제공 코드/1-9/sort_qsort.c
@@ -11,9 +11,9 @@
 
 int compare(const void *arg1, const void *arg2)
 {
-    if (*(double *)arg1 > *(double *)arg2) return 1;
-    else if (*(double *)arg1 < *(double *)arg2) return -1;
-    else return 0;
+    if (*(double *)arg1 > *(double *)arg2) return 1; // arg1이 더 크면 1
+    else if (*(double *)arg1 < *(double *)arg2) return -1; // arg2가 더 크면 -1
+    else return 0;  // 같으면 0
 }
 void main()
 {
@@ -23,7 +23,8 @@ void main()
     for (int i=0; i < 9; i++)
         printf("%4.1f ", list[i]);
 
-    qsort((void *)list, 9, sizeof(double), compare);
+    // 첫번째 인자 => DB, 두번째 인자 => 갯수, 세번째 인자 => 크기 네번째 인자 => 함수의 주소
+    qsort((void *)list, 9, sizeof(double), compare); // stdlib.h의 qsort() 함수 사용, 라이브러리 사용
     printf("\n정렬 후: ");
     for (int i=0; i<9; i++)
         printf("%4.1f ", list[i]);
